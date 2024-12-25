57377128ca97f4917b4d522627fb7748e7b127f0
Tanzim-ahmed-ratin <ratinmd64@gamil.com>
2024-12-25 21:11:37 +0600

    single line declare three variables

--- ratin.js
+++ ratin.js
@@ -1,4 +1,12 @@
 // Create a variable called carName, assign the value Volvo to it.
 
-let carName = "Volvo";
-console.log(carName);
+// let carName = "Volvo";
+// console.log(carName);
+
+// On one single line, declare three variables with the following names and values:
+
+let firstName = "John", lastName = "Doe", age = 35;
+ console.log(firstName);
+ 
+
+
