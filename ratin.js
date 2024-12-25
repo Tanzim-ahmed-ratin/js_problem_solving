08fe7ade5b432373b2460e0959a78ed7b02addf6
Tanzim-ahmed-ratin <ratinmd64@gamil.com>
2024-12-25 21:23:57 +0600

    use the assignment operator

--- ratin.js
+++ ratin.js
@@ -5,8 +5,13 @@
 
 // On one single line, declare three variables with the following names and values:
 
-let firstName = "John", lastName = "Doe", age = 35;
- console.log(firstName);
+// let firstName = "John", lastName = "Doe", age = 35;
+//  console.log(firstName);
  
+// 1. Use the correct **assignment operator** that will result in `x` being `50` (same as `x = x * y`).
 
+let x = 10;
+let y = 5;
+let a = x * y;
+console.log(a);
 
