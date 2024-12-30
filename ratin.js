78b1518a6d178a7e7b6047511baebad9d0d8c0aa
Tanzim-ahmed-ratin <ratinmd64@gamil.com>
2024-12-30 21:49:33 +0600

    calculate the factorial of a number

--- ratin.js
+++ ratin.js
@@ -139,9 +139,19 @@
 
 // Write a function to check if an array is empty. The function should take a single argument, which is the array.
 
-function isArrayEmpty(arr) {
-    return arr.length === 0;
+// function isArrayEmpty(arr) {
+//     return arr.length === 0;
+//   }
+  
+//   console.log(isArrayEmpty([]));
+//   console.log(isArrayEmpty([1, 2, 3]))
+
+// Write a function to return the factorial of a number. The function should take a single argument, which is the number.
+
+function factorialize(num) {
+    if (num === 0 || num === 1) return 1;
+    return num * factorialize(num - 1);
   }
   
-  console.log(isArrayEmpty([]));
-  console.log(isArrayEmpty([1, 2, 3]))
+  console.log(factorialize(5));
+  console.log(factorialize(7));
