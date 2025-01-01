7168b66315e16fc364f9f00b079e0c68ad97acd7
Tanzim-ahmed-ratin <ratinmd64@gamil.com>
2025-01-01 13:14:05 +0600

    find the sum of all elements in an array

--- ratin.js
+++ ratin.js
@@ -203,9 +203,19 @@
 
 //  Write a function to get the first character of a string. The function should take a single argument, which is the string.
 
-function getFirstCharacter(str) {
-  return str[0];
+// function getFirstCharacter(str) {
+//   return str[0];
+// }
+
+// console.log(getFirstCharacter("hello"));
+// console.log(getFirstCharacter("world"));
+
+// Write a function to find the sum of all elements in an array. The function should take a single argument, which is the array.
+
+function sumArray(arr) {
+  return arr.reduce((sum, current) => sum + current, 0);
 }
 
-console.log(getFirstCharacter("hello"));
-console.log(getFirstCharacter("world"));
+console.log(sumArray([1, 2, 3, 4]));
+console.log(sumArray([-1, -2, -3, -4]));
+console.log(sumArray([1.5, 2.5, 3.5]));
